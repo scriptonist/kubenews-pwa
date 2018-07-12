94388832c7f8dfe7c6b970a81e4f0ca7bcdbb071
ajomadlabs <ajojohn@madlabs.xyz>
2018-07-12 21:01:01 +0530

    :smiley: Fixes made to service workers

--- build/service-worker-prod.js
+++ build/service-worker-prod.js
@@ -60,6 +60,35 @@
     e.preventDefault();
     // Stash the event so it can be triggered later.
     deferredPrompt = e;
+    btnAdd.style.display = 'block';
     return false;
   });
+
+  btnSave.addEventListener('click', function() {
+    if(deferredPrompt !== undefined) {
+      // The user has had a positive interaction with our app and Chrome
+      // has tried to prompt previously, so let's show the prompt.
+      deferredPrompt.prompt();
+  
+      // Follow what the user has done with the prompt.
+      deferredPrompt.userChoice.then(function(choiceResult) {
+  
+        console.log(choiceResult.outcome);
+  
+        if(choiceResult.outcome == 'dismissed') {
+          console.log('User cancelled home screen install');
+        }
+        else {
+          console.log('User added to home screen');
+        }
+  
+        // We no longer need the prompt.  Clear it up.
+        deferredPrompt = null;
+      });
+    }
+  });
+
+  window.addEventListener('appinstalled', (evt) => {
+    app.logEvent('kubenews', 'installed');
+  });  
 })();
