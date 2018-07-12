08da330b6a450556fdd62ea3ca25b003c895de56
ajomadlabs <ajojohn@madlabs.xyz>
2018-07-12 14:49:21 +0530

    :smiley: Fixed ServiceWorker Bug

--- build/service-worker-prod.js
+++ build/service-worker-prod.js
@@ -52,4 +52,14 @@
         });
       }
   });
+
+  let deferredPrompt;
+
+  window.addEventListener('beforeinstallprompt', function(e) {
+    console.log('beforeinstallprompt Event fired');
+    e.preventDefault();
+    // Stash the event so it can be triggered later.
+    deferredPrompt = e;
+    return false;
+  });
 })();
