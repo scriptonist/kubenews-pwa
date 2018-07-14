0216d9b2e977c22dae1dffabd529f58ac5994828
ajomadlabs <ajojohn@madlabs.xyz>
2018-07-14 10:13:23 +0530

    :smiley: Added Services

---
+++ src/services/newsClientServices.js
@@ -0,0 +1,29 @@
+import Api from '@/services/Api.js'
+
+export default {
+  // ----- Fetch Daily Issue -----
+  fetchIssue () {
+    return Api().get()
+  },
+  // ----- End -----
+  // ----- Fetch Issue Archive -----
+  fetchArchive () {
+    return Api().get()
+  },
+  // ----- End -----
+  // ----- Fetch Particular Issue -----
+  fetchOneIssue () {
+    return Api().get()
+  },
+  // ----- End -----
+  // ----- Email Subscription -----
+  subscribe (userData) {
+    return Api().post('a', userData)
+  },
+  // ----- End -----
+  // ----- Submit User Story -----
+  submitStory (userData) {
+    return Api().post('a', userData)
+  }
+  // ----- End -----
+}
