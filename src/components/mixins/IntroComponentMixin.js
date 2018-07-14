f343208d539bfc804fc4e62b7c5c8bb34d00a69b
ajomadlabs <ajojohn@madlabs.xyz>
2018-07-14 10:16:45 +0530

    :hammer: Cleaned all Mixins

--- src/components/mixins/IntroComponentMixin.js
+++ src/components/mixins/IntroComponentMixin.js
@@ -6,24 +6,7 @@ export default {
       id: 0
     }
   },
-  mounted () {
-    this.move()
-  },
   methods: {
-    move: function () {
-      this.width = 1
-      this.id = setInterval(this.frame, 15)
-    },
-    frame: function () {
-      let elem = document.getElementById('myBar')
-      if (this.width >= 100) {
-        clearInterval(this.id)
-        this.goNext('news')
-      } else {
-        this.width++
-        elem.style.width = this.width + '%'
-      }
-    },
     goNext: function (paths) {
       this.$router.push({path: '/' + paths})
     }
